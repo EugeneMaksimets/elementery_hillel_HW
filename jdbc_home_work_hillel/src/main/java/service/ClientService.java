2ebb31922020ddddd683e0e34b36482fc61ac362
EugeneMaksimets <76277201+EugeneMaksimets@users.noreply.github.com>
2021-03-21 11:21:16 +0200

    rewrite imports

--- jdbc_home_work_hillel/src/main/java/service/ClientService.java
+++ jdbc_home_work_hillel/src/main/java/service/ClientService.java
@@ -2,8 +2,11 @@ package service;
 
 import database.Database;
 import entity.Clients;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
